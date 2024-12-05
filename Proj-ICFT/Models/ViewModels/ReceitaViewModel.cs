9cbb2fc923b9c059bdac0224ab40df15a7c688fb
Julian Freitas Reina <jreina@fapex.org.br>
2024-12-05 16:44:36 -0300

    --começando detalhar paciente

--- Proj-ICFT/Models/ViewModels/ReceitaViewModel.cs
+++ Proj-ICFT/Models/ViewModels/ReceitaViewModel.cs
@@ -5,6 +5,8 @@
         public string NomePaciente { get; set; } //caso precise salvar, transformar em entidade
         public List<RemedioViewModel> RemedioList { get; set; }
 
+        public Alerta? Alerta { get; set; }
+
         public int PesoTotal => RemedioList.Sum(r => r.PesoTotal);
 
 
