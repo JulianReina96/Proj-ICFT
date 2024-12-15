ba88461ae6a08069c1d41add81b71f2d915032f3
julian <julianreina@gmail.com>
2024-12-15 11:49:37 -0300

    --revert to relative path

--- Proj-ICFT/Models/ModelUrl/ModelUrlJson.cs
+++ Proj-ICFT/Models/ModelUrl/ModelUrlJson.cs
@@ -2,9 +2,6 @@
 {
     public class ModelUrlJson
     {
-        //public string UrlJson = Path.GetFullPath("..\\JsonFire\\jsonFire.json");
-        public string UrlJson = "D:\\" + Path.GetFullPath("..\\JsonFire\\jsonFire.json");
-
-        
+        public string UrlJson = @"JsonFire\\jsonFire.json";
     }
 }
