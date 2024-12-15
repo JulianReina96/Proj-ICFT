8789e4f0d5ae5b9b2dd887d2e58aa81fd6eded06
julian <julianreina@gmail.com>
2024-12-15 12:20:29 -0300

    --nova publicacao

--- Proj-ICFT/Models/ModelUrl/ModelUrlJson.cs
+++ Proj-ICFT/Models/ModelUrl/ModelUrlJson.cs
@@ -3,7 +3,7 @@
     public class ModelUrlJson
     {
 
-        public string UrlJson = Environment.GetEnvironmentVariable("HOME") + @"JsonFire\\jsonFire.json";
-        //public string UrlJson = @"JsonFire\\jsonFire.json";
+        //public string UrlJson = Environment.GetEnvironmentVariable("HOME") + @"JsonFire\\jsonFire.json";
+        public string UrlJson = @"JsonFire\\jsonFire.json";
     }
 }
