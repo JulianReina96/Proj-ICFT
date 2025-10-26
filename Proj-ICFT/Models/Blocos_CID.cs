f9e9ab9e31dff19eb64bcb5b3c538f9b56900dae
julian <julianreina@gmail.com>
2025-10-26 13:02:21 -0300

    inserção de entidades CID-11 e Medicamentos Anvisa

---
+++ Proj-ICFT/Models/Blocos_CID.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Proj_ICFT.Models
+{
+    [Serializable]
+    [Table("Blocos_CID")]
+    public class Blocos_CID
+    {
+
+        public int Id { get; set; }
+        public string BlockId { get; set; }
+        public string Title { get; set; }
+        public string TitleEN { get; set; }
+        public string ChapterNo { get; set; }
+        public string Grouping1 { get; set; }
+        public string Grouping2 { get; set; }
+        
+
+
+    }
+}
